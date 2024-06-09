87946964cd64d2f624c1270447c0fff4d1108e51
pokapachka <pachkaosokin@yandex.ru>
2024-06-09 19:12:04 +0500

    Программирование страницы графика. Осокин

--- Pages/Chart.xaml.cs
+++ Pages/Chart.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ПР53_Осокин.Pages
 {
@@ -20,9 +21,87 @@ namespace ПР53_Осокин.Pages
     /// </summary>
     public partial class Chart : Page
     {
-        public Chart()
+        public MainWindow mainWindow;
+        public double maxValue = 0;
+        public double actualHeightCanvas = 0;
+        public double averageValue = 0;
+        public DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
+        public Chart(MainWindow mainWindow)
         {
             InitializeComponent();
+            this.mainWindow = mainWindow;
+            actualHeightCanvas = (mainWindow.Height / 2) - 50d;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
+            dispatcherTimer.Tick += CreateNewValue;
+            dispatcherTimer.Start();
+            CreateChart();
+            ColorChart();
+        }
+        public void CreateChart()
+        {
+            canvas.Children.Clear();
+            for (int i = 0; i < mainWindow.pointsInfo.Count; i++)
+            {
+                if (mainWindow.pointsInfo[i].value > maxValue)
+                {
+                    maxValue = mainWindow.pointsInfo[i].value;
+                }
+            }
+            for (int i = 0; i < mainWindow.pointsInfo.Count; i++)
+            {
+                Line line = new Line();
+                line.X1 = i * 20;
+                line.X2 = (i + 1) * 20;
+                if (i == 0) line.Y1 = actualHeightCanvas;
+                else line.Y1 = actualHeightCanvas - ((mainWindow.pointsInfo[(i - 1)].value / maxValue) * actualHeightCanvas);
+                line.Y2 = actualHeightCanvas - ((mainWindow.pointsInfo[i].value / maxValue) * actualHeightCanvas);
+                line.StrokeThickness = 2;
+                mainWindow.pointsInfo[i].line = line;
+                canvas.Children.Add(line);
+            }
+        }
+        public void CreatePoint()
+        {
+            Line line = new Line();
+            line.X1 = (mainWindow.pointsInfo.Count - 1) * 20;
+            line.X2 = mainWindow.pointsInfo.Count * 20;
+            line.Y1 = actualHeightCanvas - ((mainWindow.pointsInfo[(mainWindow.pointsInfo.Count - 2)].value / maxValue) * actualHeightCanvas);
+            line.Y2 = actualHeightCanvas - ((mainWindow.pointsInfo[(mainWindow.pointsInfo.Count - 1)].value / maxValue) * actualHeightCanvas);
+            line.StrokeThickness = 2;
+            mainWindow.pointsInfo[(mainWindow.pointsInfo.Count - 1)].line = line;
+            canvas.Children.Add(line);
+        }
+        public void ControlCreateChart()
+        {
+            double value = mainWindow.pointsInfo[mainWindow.pointsInfo.Count - 1].value;
+            if (value < maxValue) CreatePoint();
+            else CreateChart();
+            ColorChart();
+        }
+        public void ColorChart()
+        {
+            double value = mainWindow.pointsInfo[mainWindow.pointsInfo.Count - 1].value;
+            for (int i = 0; i < mainWindow.pointsInfo.Count; i++)
+            {
+                averageValue += mainWindow.pointsInfo[i].value;
+            }
+            average_value = averageValue / mainWindow.pointsInfo.Count;
+            for (int i = 0; i < mainWindow.pointsInfo.Count; i++)
+            {
+                if (value < averageValue) mainWindow.pointsInfo[i].line.Stroke = Brushes.Red;
+                else mainWindow.pointsInfo[i].line.Stroke = Brushes.Green;
+            }
+            canvas.Width = mainWindow.pointsInfo.Count * 20 + 300;
+            scroll.ScrollToHorizontalOffset(canvas.Width);
+            current_value.Content = "Тек. знач: " + Math.Round(value, 2);
+            average_value.Content = "Сред. знач: " + Math.Round(averageValue, 2);
+        }
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            actualHeightCanvas = (mainWindow.Height / 2) - 50d;
+            CreateChart();
+            ColorChart();
         }
     }
 }
